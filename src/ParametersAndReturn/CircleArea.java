6f19e293bf4735a2b6242e811161fe25b7ff9d9f
Dzvina <matyashdo@gmail.com>
2018-12-19 10:59:34 +0100

    new exercise

--- src/ParametersAndReturn/CircleArea.java
+++ src/ParametersAndReturn/CircleArea.java
@@ -2,5 +2,12 @@ package ParametersAndReturn;
 
 public class CircleArea {
 
+    public static void circleArea(double radius){
+        double area = Math.PI * radius * radius;
+        System.out.println(area);
+    }
 
+    public static void main(String[] args) {
+        circleArea(2);
+    }
 }
