3c4ec16be835e5ec3e73eaca96963b7ba853b066
Dzvina <matyashdo@gmail.com>
2018-12-19 14:38:45 +0100

    Sum Of Digits

---
+++ src/ParametersAndReturn/SumOfDigits.java
@@ -0,0 +1,22 @@
+package ParametersAndReturn;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+    public static int sumOfDigits(int digits) {
+        int sum = 0;
+        while (digits > 0) {
+            sum += digits % 10;
+            digits /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write number ");
+        int number = scanner.nextInt();
+        System.out.println("Digit sum is " + sumOfDigits(number));
+    }
+}
