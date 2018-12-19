8de8f72378af5981d4e1db15d637dea7b0441c2e
Dzvina <matyashdo@gmail.com>
2018-12-19 11:09:38 +0100

    Average Of 3

---
+++ src/ParametersAndReturn/AverageOf3.java
@@ -0,0 +1,12 @@
+package ParametersAndReturn;
+
+public class AverageOf3 {
+
+    public static int averageOf3 ( int a, int b, int c){
+        return (a + b + c)/3;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(averageOf3(4,7,13));
+    }
+}
