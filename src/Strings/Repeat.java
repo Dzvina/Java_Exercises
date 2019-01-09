6a5437772d534925e56c59fdabc8b461d2a52f7b
Dzvina <matyashdo@gmail.com>
2019-01-09 12:47:52 +0100

    Repeat

---
+++ src/Strings/Repeat.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class Repeat {
+
+    public static String repeat(String word, int num) {
+        while (num > 0) {
+            System.out.print(word);
+            num--;
+        }
+
+        return word;
+    }
+
+    public static void main(String[] args) {
+        repeat("Ball",3);
+    }
+}
