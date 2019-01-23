607eafde2e1e4592b4a8f060090c8c84c6382f2a
Dzvina <matyashdo@gmail.com>
2019-01-23 13:40:35 +0100

    Squared Array

---
+++ src/Arrays/SquaredArray.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class SquaredArray {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            int result = i * i;
+            System.out.print(result + "  ");
+        }
+    }
+}
